acda469ac7dd9783748915b15c59d52266827f02
Glen Daniels <glen.daniels@accenture.com>
2020-07-17 11:19:30 -0400

    Initial commit, update for Android and README edits

---
+++ app/src/test/java/supermarket/FakeCatalog.kt
@@ -0,0 +1,18 @@
+package supermarket
+
+import supermarket.model.Product
+import supermarket.model.SupermarketCatalog
+
+class FakeCatalog : SupermarketCatalog {
+    private val products = HashMap<String, Product>()
+    private val prices = HashMap<String, Double>()
+
+    override fun addProduct(product: Product, price: Double) {
+        this.products[product.name] = product
+        this.prices[product.name] = price
+    }
+
+    override fun getUnitPrice(p: Product): Double {
+        return this.prices[p.name]!!
+    }
+}
---
+++ app/src/test/java/supermarket/SupermarketTest.kt
@@ -0,0 +1,32 @@
+package supermarket
+
+import org.junit.Test
+import supermarket.model.Product
+import supermarket.model.ProductUnit
+import supermarket.model.ShoppingCart
+import supermarket.model.SpecialOfferType
+import supermarket.model.Teller
+
+class SupermarketTest {
+
+    @Test
+    fun testSomething() {
+        val catalog = FakeCatalog()
+        val toothbrush = Product("toothbrush", ProductUnit.Each)
+        catalog.addProduct(toothbrush, 0.99)
+        val apples = Product("apples", ProductUnit.Kilo)
+        catalog.addProduct(apples, 1.99)
+
+        val cart = ShoppingCart()
+        cart.addItemQuantity(apples, 2.5)
+        cart.addItemQuantity(toothbrush, 1.0)
+
+        val teller = Teller(catalog)
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0)
+
+        val receipt = teller.checksOutArticlesFrom(cart)
+
+        // TODO: This just prints a receipt to give you an idea how the code works.
+        println(ReceiptPrinter().printReceipt(receipt))
+    }
+}
